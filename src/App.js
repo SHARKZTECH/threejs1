4c59a4cdfacf39af442c758cfd24e629fbb694f1
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-07-10 11:54:18 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import Model from "./Model";
+import Model from "./Model"; /* highlight-line */
 
 export default function App() {
   return (
