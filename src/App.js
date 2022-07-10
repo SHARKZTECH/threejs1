3b6830079c45e3195cf826425493f0fc4e100ac4
Sharkz Reigns <87173929+SHARKZTECH@users.noreply.github.com>
2022-07-10 11:00:30 -0400

    rendering Ready Player Me Avatar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Model from "./Model";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Canvas
+      camera={{ position: [2, 0, 12.25], fov: 15 }}
+      style={{
+        backgroundColor: "#111a21",
+        width: "100vw",
+        height: "100vh"
+      }}
+    >
+      <ambientLight intensity={1.25} />
+      <ambientLight intensity={0.1} />
+      <directionalLight intensity={0.4} />
+      <Suspense fallback={null}>
+        <Model position={[0.025, -0.9, 0]} />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
   );
 }
